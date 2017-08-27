3c2df3d701e5c23e96624d5261f4cb40841916e1
drobnik <drobniklukasz@gmail.com>
2017-08-27 17:56:19 +0200

    minor changes

--- routes/app.js
+++ routes/app.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var Group = require('../models/group');
+// var Group = require('../models/group');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Group.findOne({}, function(err, doc) {
-    if (err) {
-      return res.send('Error!');
-    }
-      res.render('index', { name: doc}); // renders the view (index page)
-  });
+  // Group.findOne({}, function(err, doc) {
+  //   if (err) {
+  //     return res.send('Error!');
+  //   }
+  //     res.render('index', { name: doc.name}); // renders the view (index page)
+  // });
+
+    res.render('index');
+
 });
 
 module.exports = router;
