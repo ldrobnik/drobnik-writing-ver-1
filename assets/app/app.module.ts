7ee55e7654f5a2448b5591f668018ddbee28a359
drobnik <drobniklukasz@gmail.com>
2017-08-29 10:20:33 +0200

    create basic structure of nav component

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; // imports a library for using Angular in a web browser
 
 import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [
+        AppComponent,
+        NavComponent
+    ],
     imports: [BrowserModule],
     bootstrap: [AppComponent] // tells Angular to bootstrap with AppComponent
 })
