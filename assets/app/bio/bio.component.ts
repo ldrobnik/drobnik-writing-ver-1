bdf99d6a4bcb3a5575e6bf8019880393727fff03
drobnik <drobniklukasz@gmail.com>
2017-08-30 15:59:26 +0200

    create sample texts and bio components

---
+++ assets/app/bio/bio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-bio',
+    templateUrl: './bio.component.html',
+    styleUrls: ['./bio.component.css']
+})
+
+export class BioComponent {
+
+}
---
+++ assets/app/texts/texts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-texts',
+    templateUrl: './texts.component.html',
+    styleUrls: ['./texts.component.css']
+})
+
+export class TextsComponent {
+
+}
