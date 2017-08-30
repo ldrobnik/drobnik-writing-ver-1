6a317f6fde8f7700eea710f2a14adf53e244fbb8
drobnik <drobniklukasz@gmail.com>
2017-08-30 16:08:38 +0200

    configure front end routing

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'; // imports a library for using Angular in a web browser
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
+import { routing } from "./app.routing";
+
 import { AppComponent } from "./app.component";
 import { NavComponent } from "./nav/nav.component";
+import { BioComponent } from "./bio/bio.component";
+import { TextsComponent } from "./texts/texts.component";
 
 @NgModule({
     declarations: [
         AppComponent,
-        NavComponent
+        NavComponent,
+        BioComponent,
+        TextsComponent
     ],
     imports: [
         BrowserModule,
-        BsDropdownModule.forRoot()
+        BsDropdownModule.forRoot(),
+        routing
     ],
     bootstrap: [AppComponent] // tells Angular to bootstrap with AppComponent
 })
--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -1,2 +1,12 @@
-import { Routes, RouterModule } from "@angular/router";
+import {Routes, RouterModule, Router} from "@angular/router";
 
+import { BioComponent } from "./bio/bio.component";
+import { TextsComponent } from "./texts/texts.component";
+
+const APP_ROUTES: Routes = [
+    {path: '', redirectTo: '/texts', pathMatch: 'full'},
+    {path: 'bio', component: BioComponent},
+    {path: 'texts', component: TextsComponent}
+];
+
+export const routing = RouterModule.forRoot(APP_ROUTES);
