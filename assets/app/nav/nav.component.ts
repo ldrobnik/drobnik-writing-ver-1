5848a9c060a9ac096912d288ccd4c96d302e5188
drobnik <drobniklukasz@gmail.com>
2017-09-04 17:56:17 +0200

    make logo update when theme is changed

--- assets/app/nav/nav.component.ts
+++ assets/app/nav/nav.component.ts
@@ -15,11 +15,12 @@ export class NavComponent implements OnInit {
 
     constructor(private appComponent: AppComponent) {
         this.theme = appComponent.theme;
-        this.logoPath = (this.theme == 1 || this.theme == 5)? 'images/logo2.png' : 'images/logo1.png';
+        this.logoPath = (this.theme == 1 || this.theme == 5) ? 'images/logo2.png' : 'images/logo1.png';
     }
 
     onChangeTheme(themeNumber) {
         this.theme = themeNumber;
+        this.logoPath = (this.theme == 1 || this.theme == 5) ? 'images/logo2.png' : 'images/logo1.png';
         this.themeChanged.emit({theme: this.theme});
     }
 
