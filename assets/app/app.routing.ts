d1ac5a5688c97633f798566371769822191c1b77
drobnik <drobniklukasz@gmail.com>
2017-09-05 08:50:27 +0200

    add some jumbotron styling

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -2,11 +2,13 @@ import {Routes, RouterModule, Router} from "@angular/router";
 
 import { BioComponent } from "./bio/bio.component";
 import { TextsComponent } from "./texts/texts.component";
+import { TextNocturine1Component } from "./texts/text-nocturine1-component";
 
 const APP_ROUTES: Routes = [
-    {path: '', redirectTo: '/texts', pathMatch: 'full'},
+    {path: '', redirectTo: '/nocturine1', pathMatch: 'full'},
     {path: 'bio', component: BioComponent},
-    {path: 'texts', component: TextsComponent}
+    {path: 'texts', component: TextsComponent},
+    {path: 'nocturine1', component: TextNocturine1Component}
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
--- assets/app/texts/text-nocturine1-component.ts
+++ assets/app/texts/text-nocturine1-component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-nocturine1',
-    templateUrl: 'text-nocturine1-template.html'
+    templateUrl: 'text-nocturine1-template.html',
+    styleUrls: ['text.component.css']
 })
 
 export class TextNocturine1Component {
