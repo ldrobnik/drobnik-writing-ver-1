c5990bf02d76ae124334a7050606fda53d128108
drobnik <drobniklukasz@gmail.com>
2017-09-05 09:57:23 +0200

    start styling the text

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -6,12 +6,15 @@ import {Component, Input, OnInit} from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+    // Property specifying the theme: 0 = nocturine, 1 = cunninghamella, 2 = vostok, 3 = omnivoria, 4 = devonian, 5 = obrovsky
+
     @Input() theme: number = 0;
 
 
+    //Method changing the theme:
+
     setTheme(newTheme: number) {
         this.theme = newTheme;
-        console.log(this.theme);
     }
 
     constructor() {
