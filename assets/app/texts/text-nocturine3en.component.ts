379e16df4f36c2b40274d6ecda23111fee5f7acd
drobnik <drobniklukasz@gmail.com>
2017-09-07 18:02:52 +0200

    set correct English and Polish navbar links

---
+++ assets/app/texts/text-nocturine3en.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: 'text-nocturine1.component.html',
+    styleUrls: ['text.component.css']
+})
+
+export class TextNocturine1Component {
+
+}
