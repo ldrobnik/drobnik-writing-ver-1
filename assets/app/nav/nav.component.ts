fa674ad93fee45031bc7d8e4ca95a4592c099c12
drobnik <drobniklukasz@gmail.com>
2017-09-11 10:53:10 +0200

    make the text tracking method add only texts that haven't been read before

--- assets/app/nav/nav.component.ts
+++ assets/app/nav/nav.component.ts
@@ -7,8 +7,6 @@ import "rxjs/add/operator/mergeMap";
 import { ResetScrollService } from "../services/reset-scroll.service";
 
 
-import { AppComponent } from "../app.component";
-
 @Component ({
     selector: 'app-nav',
     templateUrl: './nav.component.html',
@@ -22,9 +20,9 @@ export class NavComponent implements OnInit {
     langEn: boolean; //specifies whether the language is English (true) or Polish (false)
     routesPl = ['nocturine1', 'nocturine2', 'nocturine3', 'cunninghamella1', 'cunninghamella2', 'vostok1', 'vostok2', 'vostok3', 'vostok4', 'biegnacyczlowiek', 'wszystkozernosc', 'moths', 'obrovsky']; //specifies all available urls of Polish texts
     routesEn = ['nocturine1/en', 'nocturine2/en', 'nocturine3/en', 'cunninghamella1/en', 'vostok1/en', 'vostok2/en', 'vostok3/en', 'vostok4/en', 'moths/en', 'obrovsky/en']; //specifies all available urls of English texts
-    randomPath : string; //holds the path to randomly chosen text
-    currentUrl: string; //holds the current path
-    equivalentPath: string; //specifies the path of the equivalent text in the other language, if exists
+    // randomPath : string; //holds the path to randomly chosen text
+    // currentUrl: string; //holds the current path
+    // equivalentPath: string; //specifies the path of the equivalent text in the other language, if exists
     visitedRoutesPl = []; //holds all visited paths to Polish texts
     visitedRoutesEn = []; //same for English texts
 
@@ -35,32 +33,33 @@ export class NavComponent implements OnInit {
 
     //method changing the language and looking for the equivalent text in the other language
     changeLang() {
-        this.currentUrl = this.router.url;
-        console.log(this.currentUrl);
+        var currentUrl = this.router.url; //holds the current path
+        var equivalentPath: string; //specifies the path of the equivalent text in the other language, if exists
+        console.log(currentUrl);
         //look for equivalent of the current text in the other language
         if (this.langEn) {
 
-            if (this.currentUrl.includes('moths')) {
-                this.equivalentPath = "cmy";
+            if (currentUrl.includes('moths')) {
+                equivalentPath = "cmy";
             } else {
-                this.equivalentPath = this.currentUrl.slice(1, -3); //cut the '/en' ending off the path
+                equivalentPath = currentUrl.slice(1, -3); //cut the '/en' ending off the path
             }
 
         } else {
-            if (this.currentUrl.includes('cmy')) {
-                this.equivalentPath = "moths/en";
+            if (currentUrl.includes('cmy')) {
+                equivalentPath = "moths/en";
             } else {
-                this.equivalentPath = this.currentUrl + "/en";
+                equivalentPath = currentUrl + "/en";
             }
         }
         this.langEn =! this.langEn; //change website language langEN - English; !langEn - Polish
 
         //for texts not having equivalents in the other language, go to random text, for the rest - go to the equivalent text path:
 
-        if (this.currentUrl.includes('cunninghamella2') || this.currentUrl.includes('wszystkozernosc') || this.currentUrl.includes('biegnacyczlowiek')) {
+        if (currentUrl.includes('cunninghamella2') || currentUrl.includes('wszystkozernosc') || currentUrl.includes('biegnacyczlowiek')) {
             this.goToRandomText();
         } else {
-            this.router.navigate([this.equivalentPath]);
+            this.router.navigate([equivalentPath]);
         }
 
         this.resetScroll();
@@ -76,16 +75,21 @@ export class NavComponent implements OnInit {
     //method to track texts read
 
     trackText(url: string) {
+        var justPathName = url.substr(1);
         if (url.includes('/en')) {
-            if (this.visitedRoutesPl.length >= this.routesPl) {
+            if (this.visitedRoutesPl.length >= this.routesPl.length) {
                 this.visitedRoutesPl = [];
             }
-            this.visitedRoutesPl.push(url);
+            if (this.visitedRoutesPl.indexOf(justPathName) === -1) {
+                this.visitedRoutesPl.push(url);
+            }
         } else {
-            if (this.visitedRoutesEn.length >= this.routesEn) {
+            if (this.visitedRoutesEn.length >= this.routesEn.length) {
                 this.visitedRoutesEn = [];
             }
-            this.visitedRoutesEn.push(url);
+            if (this.visitedRoutesEn.indexOf(justPathName) === -1) {
+                this.visitedRoutesEn.push(url);
+            }
         }
         console.log(this.visitedRoutesPl);
         console.log(this.visitedRoutesEn);
@@ -94,13 +98,15 @@ export class NavComponent implements OnInit {
 
     goToRandomText() {
 
+        var randomPath : string; //holds the path to randomly chosen text
+
         if (this.router.url.includes('/en')) {
-            this.randomPath = this.routesEn[Math.floor(Math.random() * this.routesEn.length)];
+            randomPath = this.routesEn[Math.floor(Math.random() * this.routesEn.length)];
         } else {
-            this.randomPath = this.routesPl[Math.floor(Math.random() * this.routesPl.length)];
+            randomPath = this.routesPl[Math.floor(Math.random() * this.routesPl.length)];
         }
         this.resetScroll();
-        this.router.navigate([this.randomPath]);
+        this.router.navigate([randomPath]);
     }
 
 
