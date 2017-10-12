f8ea6e2d79813992250e230dc772b6debe4f4130
drobnik <drobniklukasz@gmail.com>
2017-10-12 15:03:48 +0200

    show slider when screen height is low

--- assets/app/app.component.ngfactory.ts
+++ assets/app/app.component.ngfactory.ts
@@ -18,17 +18,24 @@ export const RenderType_AppComponent:i1.RendererType2 = i1.ɵcrt({encapsulation:
     data:{}});
 export function View_AppComponent_0(_l:any):i1.ɵViewDefinition {
   return i1.ɵvid(0,[(_l()(),i1.ɵeld(0,(null as any),(null as any),1,'app-nav',([] as any[]),
-      (null as any),(null as any),(null as any),i2.View_NavComponent_0,i2.RenderType_NavComponent)),
-      i1.ɵdid(114688,(null as any),0,i3.NavComponent,[i4.Router,i4.ActivatedRoute],
-          (null as any),(null as any)),(_l()(),i1.ɵted((null as any),['\n'])),(_l()(),
-          i1.ɵeld(16777216,(null as any),(null as any),1,'router-outlet',([] as any[]),
-              (null as any),(null as any),(null as any),(null as any),(null as any))),
-      i1.ɵdid(212992,(null as any),0,i4.RouterOutlet,[i4.ChildrenOutletContexts,i1.ViewContainerRef,
-          i1.ComponentFactoryResolver,[8,(null as any)],i1.ChangeDetectorRef],(null as any),
-          (null as any))],(_ck,_v) => {
-    _ck(_v,1,0);
-    _ck(_v,4,0);
-  },(null as any));
+      (null as any),[['window','resize']],(_v,en,$event) => {
+        var ad:boolean = true;
+        if (('window:resize' === en)) {
+          const pd_0:any = ((<any>i1.ɵnov(_v,1).onResize($event)) !== false);
+          ad = (pd_0 && ad);
+        }
+        return ad;
+      },i2.View_NavComponent_0,i2.RenderType_NavComponent)),i1.ɵdid(114688,(null as any),
+      0,i3.NavComponent,[i4.Router,i4.ActivatedRoute],(null as any),(null as any)),
+      (_l()(),i1.ɵted((null as any),['\n'])),(_l()(),i1.ɵeld(16777216,(null as any),
+          (null as any),1,'router-outlet',([] as any[]),(null as any),(null as any),
+          (null as any),(null as any),(null as any))),i1.ɵdid(212992,(null as any),
+          0,i4.RouterOutlet,[i4.ChildrenOutletContexts,i1.ViewContainerRef,i1.ComponentFactoryResolver,
+              [8,(null as any)],i1.ChangeDetectorRef],(null as any),(null as any))],
+      (_ck,_v) => {
+        _ck(_v,1,0);
+        _ck(_v,4,0);
+      },(null as any));
 }
 export function View_AppComponent_Host_0(_l:any):i1.ɵViewDefinition {
   return i1.ɵvid(0,[(_l()(),i1.ɵeld(0,(null as any),(null as any),1,'my-app',([] as any[]),
@@ -40,4 +47,4 @@ export function View_AppComponent_Host_0(_l:any):i1.ɵViewDefinition {
 }
 export const AppComponentNgFactory:i1.ComponentFactory<i5.AppComponent> = i1.ɵccf('my-app',
     i5.AppComponent,View_AppComponent_Host_0,{langEn:'langEn'},{},([] as any[]));
-//# sourceMappingURL=data:application/json;base64,eyJmaWxlIjoiRTovd2ViZGV2L2x1a2Fzei1kcm9ibmlrLmNvbS9sdWthc3otZHJvYm5pay5jb20vYXNzZXRzL2FwcC9hcHAuY29tcG9uZW50Lm5nZmFjdG9yeS50cyIsInZlcnNpb24iOjMsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIm5nOi8vL0U6L3dlYmRldi9sdWthc3otZHJvYm5pay5jb20vbHVrYXN6LWRyb2JuaWsuY29tL2Fzc2V0cy9hcHAvYXBwLmNvbXBvbmVudC50cyIsIm5nOi8vL0U6L3dlYmRldi9sdWthc3otZHJvYm5pay5jb20vbHVrYXN6LWRyb2JuaWsuY29tL2Fzc2V0cy9hcHAvYXBwLmNvbXBvbmVudC5odG1sIiwibmc6Ly8vRTovd2ViZGV2L2x1a2Fzei1kcm9ibmlrLmNvbS9sdWthc3otZHJvYm5pay5jb20vYXNzZXRzL2FwcC9hcHAuY29tcG9uZW50LnRzLkFwcENvbXBvbmVudF9Ib3N0Lmh0bWwiXSwic291cmNlc0NvbnRlbnQiOlsiICIsIjxhcHAtbmF2PjwvYXBwLW5hdj5cclxuPHJvdXRlci1vdXRsZXQ+PC9yb3V0ZXItb3V0bGV0PiIsIjxteS1hcHA+PC9teS1hcHA+Il0sIm1hcHBpbmdzIjoiQUFBQTs7Ozs7Ozs7Ozs7Ozs7Ozs7OztvQkNBQTtNQUFBO2FBQUE7VUFBQSw2QkFBbUIsdUNBQ25CO2lCQUFBO2NBQUE7YUFBQTtzQ0FBQTtVQUFBO0lBREE7SUFDQTs7OztvQkNEQTtNQUFBO2FBQUE7a0JBQUE7SUFBQTs7OzsifQ==
+//# sourceMappingURL=data:application/json;base64,eyJmaWxlIjoiRTovd2ViZGV2L2x1a2Fzei1kcm9ibmlrLmNvbS9sdWthc3otZHJvYm5pay5jb20vYXNzZXRzL2FwcC9hcHAuY29tcG9uZW50Lm5nZmFjdG9yeS50cyIsInZlcnNpb24iOjMsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIm5nOi8vL0U6L3dlYmRldi9sdWthc3otZHJvYm5pay5jb20vbHVrYXN6LWRyb2JuaWsuY29tL2Fzc2V0cy9hcHAvYXBwLmNvbXBvbmVudC50cyIsIm5nOi8vL0U6L3dlYmRldi9sdWthc3otZHJvYm5pay5jb20vbHVrYXN6LWRyb2JuaWsuY29tL2Fzc2V0cy9hcHAvYXBwLmNvbXBvbmVudC5odG1sIiwibmc6Ly8vRTovd2ViZGV2L2x1a2Fzei1kcm9ibmlrLmNvbS9sdWthc3otZHJvYm5pay5jb20vYXNzZXRzL2FwcC9hcHAuY29tcG9uZW50LnRzLkFwcENvbXBvbmVudF9Ib3N0Lmh0bWwiXSwic291cmNlc0NvbnRlbnQiOlsiICIsIjxhcHAtbmF2PjwvYXBwLW5hdj5cclxuPHJvdXRlci1vdXRsZXQ+PC9yb3V0ZXItb3V0bGV0PiIsIjxteS1hcHA+PC9teS1hcHA+Il0sIm1hcHBpbmdzIjoiQUFBQTs7Ozs7Ozs7Ozs7Ozs7Ozs7OztvQkNBQTtNQUFBO1FBQUE7UUFBQTtVQUFBO1VBQUE7UUFBQTtRQUFBO01BQUEsNkRBQUE7TUFBQTtNQUFtQix1Q0FDbkI7VUFBQTtVQUFBLG1EQUFBO1VBQUE7Y0FBQTs7UUFEQTtRQUNBOzs7O29CQ0RBO01BQUE7YUFBQTtrQkFBQTtJQUFBOzs7OyJ9
